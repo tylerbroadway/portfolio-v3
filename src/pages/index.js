dcafab0cedf19894fd7a0f719f0609750e4cf450
tyler broadway <tbroadway96@gmail.com>
2021-03-09 15:02:28 -0600

    Fixed build issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,43 +4,58 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="flex flex-col h-full w-3/4 mx-12 py-10">
-      <h1 className="text-4xl text-offWhite">
-        Hey{" "}
-        <span role="img" aria-label="Hand wave emoji">
-          👋
-        </span>{" "}
-        I'm Tyler Broadway.
-      </h1>
-      <p className="my-6 text-3xl text-teal">
-        I build cool websites using cool tech.
-      </p>
-      <p className="my-2 text-xl text-offWhite">
-        I'm a software engineer with experience building full-stack
-        applications, modern websites and custom REST APIs. I have a passion for
-        making websites that are as simple and easy to use as possible, without
-        taking away from their functionality.
-      </p>
-      <p className="my-4 text-xl text-offWhite">
-        I specialize in serverless architecture, headless websites and the
-        JAMstack. Every application I build is secure, high-performing and easy
-        to maintain.
-      </p>
-      <div className="flex flex-row justify-around items-center mt-8 w-1/3">
-        <div className="flex justify-center items-center h-14 w-1/2 rounded bg-teal text-obsidian text-xl hover:">
-          <a href="#" target="_blank">
+const IndexPage = () => {
+  const openResume = () => {
+    if (typeof window !== "undefined") {
+      window.open(
+        "https://drive.google.com/file/d/1ywAHWqmNB_VM0jpoJPMWPYe1R7ef8pKZ/view?usp=sharing"
+      )
+    }
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="flex flex-col h-full w-3/4 mx-12 py-10">
+        <h1 className="text-4xl text-offWhite">
+          Hey{" "}
+          <span role="img" aria-label="Hand wave emoji">
+            👋
+          </span>{" "}
+          I'm Tyler Broadway.
+        </h1>
+        <p className="my-6 text-3xl text-teal">
+          I build cool websites using cool tech.
+        </p>
+        <p className="my-2 text-xl text-offWhite">
+          I'm a software engineer with experience building full-stack
+          applications, modern websites and custom REST APIs. I have a passion
+          for making websites that are as simple and easy to use as possible,
+          without taking away from their functionality.
+        </p>
+        <p className="my-4 text-xl text-offWhite">
+          I specialize in serverless architecture, headless websites and the
+          JAMstack. Every application I build is secure, high-performing and
+          easy to maintain.
+        </p>
+        <div className="flex flex-row justify-around items-center mt-8 w-1/3">
+          <div
+            role="button"
+            tabIndex={0}
+            aria-pressed="false"
+            onClick={() => openResume()}
+            onKeyDown={e => openResume()}
+            className="flex justify-center items-center h-14 w-1/2 rounded bg-teal text-obsidian text-xl hover:"
+          >
             View Resumé
-          </a>
-        </div>
-        <div className="text-teal text-xl">
-          <Link to="/projects/">My Work</Link>
+          </div>
+          <div className="text-teal text-xl">
+            <Link to="/projects/">My Work</Link>
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default IndexPage
