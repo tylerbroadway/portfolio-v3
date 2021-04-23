5cb941164619dab6547b1c20d87722537b770e94
tyler broadway <tbroadway96@gmail.com>
2021-04-22 20:31:23 -0500

    header update

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = () => (
   <header className="flex flex-row justify-between h-20 bg-charcoal">
     <div className="self-center ml-8 text-4xl text-teal font-light tracking-wider">
-      TB
+      {`<TB />`}
     </div>
     <div className="flex flex-row justify-around items-center w-1/3 px-6 py-auto">
       <div className="text-xl text-teal font-light tracking-wide transform hover:scale-110">
